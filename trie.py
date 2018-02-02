ffe01227a6aaab55e56d2afb4875df2fd56d6cae
Callum Hart <callumhartemail@gmail.com>
2018-02-02 16:51:46 +0000

    Corrected Trie Implementation
    I was not changing current node to next node after creating a new node.
    
    Changed verification condition to check all words are in the trie, as
    opposed to just checking one, which caused problems before.

--- trie.py
+++ trie.py
@@ -1,14 +1,8 @@
-# 'The unique node with no outedges
-# is given an index of zero by convention.'
-print(55)
 from trie_node import TrieNode
 
 with open('dict.txt') as f:
     content = f.readlines()
     dicto = [x.strip() for x in content]
-
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
 
 class Trie:
     def __init__(self, build_dict=None):
@@ -29,20 +23,6 @@ class Trie:
             Node.terminal = True
 
 
-
-    def get_adjacents(self, current):
-        adjacent = {}
-        for char in alphabet:
-            new_prefix = current.label + char
-            if any((word.startswith(new_prefix) for word in build_dict)):
-                terminal = True if new_prefix in build_dict else False
-                if terminal:
-                    print(new_prefix)
-                adjacent[char]= TrieNode(new_prefix, terminal)
-        for next_node in adjacent.values():
-            next_node.next_nodes = self.get_adjacents(next_node)
-        return adjacent
-
     def construct(self, build_dict):
         for word in build_dict:
             self.insert(word)
@@ -59,7 +39,7 @@ class Trie:
             return True
         else:
             return False
-print(1)
+
+
 my_trie = Trie(dicto)
-print(2)
-print(my_trie.contains('abcdefg'))
+print(all([my_trie.contains(word) for word in dicto]))
