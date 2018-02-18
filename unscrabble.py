18aace8546587e48706be9dc2ae5eb8ac9ff8191
Callum Hart <callumhartemail@gmail.com>
2018-02-18 00:15:24 +0000

    Score moved inside legal_plays

--- unscrabble.py
+++ unscrabble.py
@@ -5,39 +5,7 @@ from operator import mul
 from sys import argv
 
 
-def score(entry, board):
-    word = entry[0]
-    dot_places = [i for i, letter in list(enumerate(word))[:len(word) - 1]
-                  if word[i + 1] == '.']
-    blank_places = [i - j for j, i in enumerate(dot_places)]
-    word = word.replace('.', '')
-    if entry[1] == 'Vertical':
-        x = entry[3]
-        y = entry[2]
     else:
-        x = entry[2]
-        y = entry[3]
-
-    sq_list = [board.get_square(x + i, y) for i in range(0, len(word))]
-    old_lms = []
-    for i in blank_places:
-        old_lms.append(sq_list[i].lm)
-        sq_list[i].lm = 0
-    hm = reduce(mul, [sq.wm for sq in sq_list], 1)
-    cross_score = sum([sq.wm *
-                       (sq.cross_score + LETTERS[word[sq.x - x]][1] * sq.lm)
-                       for sq in sq_list if sq.empty and sq.cross_score > 0])
-    hori_score = hm * sum([LETTERS[word[sq.x - x]][1] * sq.lm
-                           for sq in sq_list])
-    emptys = sum(1 for sq in sq_list if sq.empty)
-    score = cross_score + hori_score
-    if emptys == 7:
-        print('BINGO:', word, entry[1], entry[2], entry[3])
-        score += 35
-    for j, i in enumerate(blank_places):
-        sq = sq_list[i]
-        sq.lm = old_lms[j]
-    return score
 
 
 def legal_plays(board, rack, lexicon):
@@ -55,6 +23,40 @@ def legal_plays(board, rack, lexicon):
                 m_score = score(x, board)
                 if m_score > 0:
                     all_plays.add((m_score, x))
+    def score(move):
+        word = move[0]
+        dot_places = [i for i, letter in list(enumerate(word))[:len(word) - 1]
+                      if word[i + 1] == '.']
+        blank_places = [i - j for j, i in enumerate(dot_places)]
+        word = word.replace('.', '')
+        if move[1] == 'Vertical':
+            x = move[3]
+            y = move[2]
+        else:
+            x = move[2]
+            y = move[3]
+
+        sq_list = [board.get_square(x + i, y) for i in range(0, len(word))]
+        old_lms = []
+        for i in blank_places:
+            old_lms.append(sq_list[i].lm)
+            sq_list[i].lm = 0
+        hm = reduce(mul, [sq.wm for sq in sq_list], 1)
+        cross_score = sum([sq.wm *
+                           (sq.cross_score + LETTERS[word[sq.x - x]][1] * sq.lm)
+                           for sq in sq_list if sq.empty and sq.cross_score > 0])
+        hori_score = hm * sum([LETTERS[word[sq.x - x]][1] * sq.lm
+                               for sq in sq_list])
+        emptys = sum(1 for sq in sq_list if sq.empty)
+        score = cross_score + hori_score
+        if emptys == 7:
+            print('BINGO:', word, move[1], move[2], move[3])
+            score += 35
+        for j, i in enumerate(blank_places):
+            sq = sq_list[i]
+            sq.lm = old_lms[j]
+        return score
+
     all_plays = set()
     get_plays(all_plays)
     board.transpose()
